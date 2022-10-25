fff82397e30a71bb11742bba64d526a0d911f5ef
Benjamin Machlev <beni251@gmail.com>
2022-10-25 22:55:13 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program8881.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome8881();
+            Wellcome3341();
+
+            Console.ReadKey();
+        }
+
+        static partial void Wellcome3341();
+        private static void Wellcome8881()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
