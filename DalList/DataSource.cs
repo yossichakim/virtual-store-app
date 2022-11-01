c26f01b83ad15a46daffd1822131e56401fa2119
yossi chakim <melon0533147693@gmail.com>
2022-11-01 22:45:25 +0200

    add config function

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -3,10 +3,27 @@
 namespace Dal;
 static class DataSource
 {
+
+    internal static class Config
+    {
+
+        //internal static int _indexProducts { set; get; } = 0;
+        //internal static int _indexOrders { set; get; } = 0;
+        //internal static int _indexOrdersItems { set; get; } = 0;
+
+        private static int _orderItemID = 0;
+
+        private static int _orderID = 0;
+
+        internal static int _orderItemIDGet => _orderItemID++;
+
+        internal static int _orderIDGet => _orderID++;
+    }
+
+
     static DataSource() { s_Initialize(); } //dtor
 
     private static readonly Random _random = new Random();
-
     internal static List<Product> Products { get; set; } = new List<Product>(50);
     internal static List<Order> Orders { get; set; } = new List<Order>(100);
     internal static List<OrderItem> OrderItems { get; set; } = new List<OrderItem>(200);
@@ -31,10 +48,24 @@ static class DataSource
     private static void initOrders()
     {
 
+        for (int i = 0; i < 40; i++)
+        {
+            Order order = new Order { OrderID = Config._orderIDGet, };
+        }
+
     }
+
+    private List<string> getFirstName()
+    {
+        return new List<string> { };
+    }
+
     private static void initOrderItems()
     {
 
     }
 
+    
+
+    
 }
