7b74d92a435b68476e6fc5cb8dfdc1fb7cd65379
Benjamin Machlev <beni251@gmail.com>
2022-11-01 22:24:18 +0200

    change the default enum to 0

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -3,7 +3,7 @@
 namespace Dal;
 static class DataSource
 {
-    static DataSource() { s_Initialize(); } //dtor
+    static DataSource() { s_Initialize(); } //ctor
 
     private static readonly Random _random = new Random();
 
@@ -13,19 +13,75 @@ static class DataSource
 
     private static void s_Initialize()
     {
-        initProducts();
+        initProductsByCategory();
         initOrders();
         initOrderItems();
     }
 
-    private static void initProducts()
+    private static void initProductsByCategory()
     {
-        int productID = 111111;
+        Dictionary<Category, List<string>> names = new Dictionary<Category, List<string>>
+        {
+            { Category.Screens, new List<string> {"galksy8", "lg7"} } ,
+              { Category.Printers, new List<string> {} },
+               { Category.Computers, new List<string> {} },
+                { Category.TV, new List<string> {} },
+                 { Category.Phones, new List<string> {} },
+        };
+
+        initProducts(names);
+    }
+
+    private static void initProducts(Dictionary<Category, List<string>> names)
+    {
+        int _initProductID = 100000;
+
+        foreach (var catgory in names)
+        {
+            int count = catgory.Value.Count();
+
+            int pre = (int)(count * 0.05) + 1;
+
+            foreach (var name in catgory.Value)
+            {
+                Product product = new Product
+                {
+                    ProductID = _initProductID++,
+                    Category = catgory.Key,
+                    Name = name,
+                    InStock = pre-- > 0 ? 0 : _random.Next(1, 70000),//ask
+                };
+
+                product.Price = product.Category switch
+                {
+                    Category.Screens => _random.Next(699, 2999),
+                    Category.Phones => _random.Next(499, 4999),
+                    Category.Computers => _random.Next(999, 7999),
+                    Category.Printers => _random.Next(399, 3999),
+                    Category.TV => _random.Next(1999, 9999),
+                    _ => throw new NotImplementedException()
+                };
+            }
+        }
+
         for (int i = 0; i < 25; i++)
         {
-            Product product = new Product { ProductID = productID++, };
+            Product product = new Product
+            {
+                ProductID = _initProductID++,
+                Category = (Category)_random.Next(0, 5),
+                InStock = i + (i % 5),//ask
+            };
 
-
+            product.Price = product.Category switch
+            {
+                Category.Screens => _random.Next(699, 2999),
+                Category.Phones => _random.Next(499, 4999),
+                Category.Computers => _random.Next(999, 7999),
+                Category.Printers => _random.Next(399, 3999),
+                Category.TV => _random.Next(1999, 9999),
+                _ => throw new NotImplementedException()
+            };
         }
     }
     private static void initOrders()
@@ -36,5 +92,4 @@ static class DataSource
     {
 
     }
-
 }
