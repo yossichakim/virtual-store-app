c236674b738bea2fc66b21b4ffe287ce142b73ed
Benjamin Machlev <beni251@gmail.com>
2022-11-01 18:39:02 +0200

    add Order Item ID

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,11 +1,14 @@
 ﻿namespace DO;
-public struct OrderItem {
+public struct OrderItem
+{
+    public int OrderItemID { get; set; }
     public int ProductID { get; set; }
     public int OrderID { get; set; }
     public float Price { get; set; }
     public int Amount { get; set; }
 
-        public override string ToString() => $@"
+    public override string ToString() => $@"
+        Order Item ID: {OrderItemID}
         Product ID: {ProductID}
         Order ID: {OrderID}
         Price: {Price}
