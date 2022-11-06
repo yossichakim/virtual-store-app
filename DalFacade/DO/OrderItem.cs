86dc4a53d6bdaa92c4437d929dc7210325f7def6
Benjamin Machlev <beni251@gmail.com>
2022-11-06 11:16:04 +0200

    Add comments

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,12 +1,35 @@
 ﻿namespace DO;
+
+/// <summary>
+/// Structure for Order Item
+/// </summary>
 public struct OrderItem
 {
+    /// <summary>
+    /// Unique ID of Order Item
+    /// </summary>
     public int OrderItemID { get; set; }
+    /// <summary>
+    /// Unique ID of Product
+    /// </summary>
     public int ProductID { get; set; }
+    /// <summary>
+    /// Unique ID of Order
+    /// </summary>
     public int OrderID { get; set; }
+    /// <summary>
+    /// Price of Product
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// Product quantity in order
+    /// </summary>
     public int Amount { get; set; }
 
+    /// <summary>
+    /// A function that overrides the existing function and prints the order item details
+    /// </summary>
+    /// <returns> Prints the order item details </returns>
     public override string ToString() => $@"
         Order Item ID: {OrderItemID}
         Product ID: {ProductID}
