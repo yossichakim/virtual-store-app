c842e8c83b411b80783106a024883b41fc9c6573
Benjamin Machlev <beni251@gmail.com>
2022-11-06 10:42:20 +0200

    Add comments

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -1,19 +1,40 @@
 ﻿using System.ComponentModel;
 
 namespace DO;
+
+/// <summary>
+/// Structure for Product
+/// </summary>
 public struct Product
 {
+    /// <summary>
+    /// Unique ID of Product
+    /// </summary>
     public int ProductID { get; set; }
+    /// <summary>
+    /// Name of Product
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Price of Product
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// Category of Product
+    /// </summary>
     public Category Category { get; set; }
+    /// <summary>
+    /// Product quantity in stock
+    /// </summary>
     public int InStock { get; set; }
 
+    /// <summary>
+    /// A function that overrides the existing function and prints the product details
+    /// </summary>
+    /// <returns> Prints the product details </returns>
     public override string ToString() => $@"
         Product ID: {ProductID}: {Name}
         Category: {Category}
         Price: {Price}
     	Amount in stock: {InStock}";
 }
-
-
