ee700795529cf26f95992c00d34556350953f4a7
Benjamin Machlev <beni251@gmail.com>
2022-11-06 11:18:07 +0200

    Add comments

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,5 +1,8 @@
 ﻿namespace DO;
 
+/// <summary>
+/// The menu of the types of departments available in the store
+/// </summary>
 public enum Category
 {
     Screens , Phones, Computers, Printers, TV
