8cdb1ae3ce4cd3a7a6015a01ddda8cc996e4a90c
Benjamin Machlev <beni251@gmail.com>
2022-11-06 10:51:57 +0200

    Add comments

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,17 +1,43 @@
 ﻿namespace DO;
+
+/// <summary>
+/// Structure for Order
+/// </summary>
 public struct Order
 {
-
+    /// <summary>
+    /// Unique ID of Order
+    /// </summary>
     public int OrderID { get; set; }
-
+    /// <summary>
+    /// Name of Customer
+    /// </summary>
     public string CustomerName { get; set; }
-
+    /// <summary>
+    /// Email of Customer
+    /// </summary>
     public string CustomerEmail { get; set; }
+    /// <summary>
+    /// Address of Customer
+    /// </summary>
     public string CustomerAddress { get; set; }
+    /// <summary>
+    /// Date of Order
+    /// </summary>
     public DateTime OrderDate { get; set; }
+    /// <summary>
+    /// Date of Ship the Order
+    /// </summary>
     public DateTime ShipDate { get; set; }
+    /// <summary>
+    /// Order delivery date
+    /// </summary>
     public DateTime DeliveryDate { get; set; }
 
+    /// <summary>
+    /// A function that overrides the existing function and prints the Order details
+    /// </summary>
+    /// <returns> Prints the order details </returns>
     public override string ToString() => $@"
         Product ID: {OrderID}
         Customer Name: {CustomerName}
