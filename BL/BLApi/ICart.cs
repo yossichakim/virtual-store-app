f7e88ff39b8573433c5652ae24bc41f8b5a91971
Benjamin Machlev <beni251@gmail.com>
2022-11-16 22:50:20 +0200

    create declaration of a function to interfaces

---
+++ BL/BLApi/ICart.cs
@@ -0,0 +1,10 @@
+﻿using BO;
+
+namespace BLApi;
+
+public interface ICart
+{
+    public Cart AddProductToCart(Cart cart, int productID);
+    public Cart UpdateAmount(Cart cart, int productID, int newAmount);
+    public void ConfirmedOrder(Cart cart, string costumerName, string costumerEmail, string costumerAddress);
+}
---
+++ BL/BLApi/IOrder.cs
@@ -0,0 +1,15 @@
+﻿using BO;
+
+namespace BLApi;
+
+public interface IOrder
+{
+    public IEnumerable<OrderForList> GetOrderList();
+    public Order GetOrderDetails(int orderID);
+    public Order ShippingUpdate(int orderID);
+    public Order DeliveryUpdate(int orderID);
+    public OrderTracking OrderTrackingManger(int orderID);
+
+    ///BONUS///
+    public void UpdateOrder(int orderID);
+}
