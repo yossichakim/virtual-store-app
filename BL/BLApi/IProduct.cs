3ca73a26fb30cc4137fa241611560f44726089ff
Benjamin Machlev <beni251@gmail.com>
2022-11-21 21:14:24 +0200

    change declaration of 2 function to BO.Product/BO.ProductItem

--- BL/BLApi/IProduct.cs
+++ BL/BLApi/IProduct.cs
@@ -5,8 +5,8 @@ namespace BLApi;
 public interface IProduct
 {
     public IEnumerable<ProductForList> GetProductList();
-    public Product GetProductManger(int productID);
-    public Product GetProductCostumer(int productID);
+    public BO.Product GetProductManger(int productID);
+    public BO.ProductItem GetProductCostumer(int productID, Cart cart);
     public void AddProduct(Product productTOAdd);
     public void RemoveProduct(int productID);
     public void UpdateProduct(Product productTOUpdate);
