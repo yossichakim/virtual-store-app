42520866249b3a6b490e514a8fa5238d8fa0f80a
Benjamin Machlev <beni251@gmail.com>
2022-11-23 19:19:51 +0200

    Adding service class

---
+++ BL/SeviceFunction/ServiceFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeviceFunction;
+
+
+internal static class ServiceFunction
+{
+    internal static bool IsValidEmail(this string mail)
+    {
+        return new EmailAddressAttribute().IsValid(mail);
+    }
+
+    internal static void CheckPositiveNumber(this int number)
+    {
+        if (number < 0)
+        {
+            throw new Exception("not positive number");
+        }
+    }
+
+    internal static void IsEmptyString(this string str)
+    {
+
+    }
+}
