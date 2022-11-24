0ac1e3b45831797d94dc8cc7521a8e10046c485f
Benjamin Machlev <beni251@gmail.com>
2022-11-24 19:32:59 +0200

    start write main menu

--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace BlTest
+﻿
+namespace BlTest;
+
+internal class Program
 {
-    internal class Program
+    private enum MainMenu
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Exit, Product, Order, Cart
+    }
+
+    private enum ProductMenu
+    {
+        GetProductList = 1, GetProductManger, GetProductCostumer, AddProduct, RemoveProduct, UpdateProduct
+    }
+
+    private enum OrderMenu
+    {
+        GetOrderList = 1, GetOrderDetails, ShippingUpdate, DeliveryUpdate, OrderTrackingManger
+    }
+
+    private enum CartMenu
+    {
+        AddProductToCart = 1, UpdateAmount, ConfirmedOrder
+    }
+
+    private static IBl bl = new Bl();
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
     }
 }
