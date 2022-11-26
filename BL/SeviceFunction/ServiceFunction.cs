17e03c2c6f4404899123b7ad88b4ce39bac3d6e3
Benjamin Machlev <beni251@gmail.com>
2022-11-27 00:02:34 +0200

    Stage2 Final commit

--- BL/SeviceFunction/ServiceFunction.cs
+++ BL/SeviceFunction/ServiceFunction.cs
@@ -2,8 +2,16 @@
 
 namespace SeviceFunction;
 
+/// <summary>
+/// An auxiliary class for the logical layer
+/// </summary>
 internal static class ServiceFunction
 {
+    /// <summary>
+    /// Accepts a string and returns whether it is a valid email address
+    /// </summary>
+    /// <param name="mail"></param>
+    /// <returns>True if the string is a valid email address, false if the string is an invalid email address</returns>
     internal static bool IsValidEmail(this string mail)
     {
         return new EmailAddressAttribute().IsValid(mail);
