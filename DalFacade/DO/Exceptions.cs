c04a3246bc8328751bd8b78d4379977b0bbbeb4b
Benjamin Machlev <beni251@gmail.com>
2022-12-12 22:09:16 +0200

    starting stage 4

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,13 +1,22 @@
 ﻿namespace DO;
 
+[Serializable]
 public class AddException : Exception
 {
     public AddException(string massage) : base($"THE {massage} YOU TRY TO ADD ALREADY EXIST")
     { }
 }
 
+[Serializable]
 public class NoFoundException : Exception
 {
     public NoFoundException(string massage) : base($"THE {massage} DOES NOT EXIST")
     { }
+}
+
+[Serializable]
+public class DalConfigException : Exception
+{
+    public DalConfigException(string msg) : base(msg) { }
+    public DalConfigException(string msg, Exception ex) : base(msg, ex) { }
 }
--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -5,8 +5,10 @@ namespace Dal;
 /// <summary>
 /// A class for the entities that inherit from the interface
 /// </summary>
-public sealed class DalList : IDal
+internal sealed class DalList : IDal
 {
+    private DalList() { }
+    public static IDal Instance { get; } = new DalList();
     public IProduct Product => new DalProduct();
     public IOrder Order => new DalOrder();
     public IOrderItem OrderItem => new DalOrderItem();
