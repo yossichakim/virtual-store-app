8ce162d2ef9c66996a7cbb80fa91dbeddd3d7255
Benjamin Machlev <beni251@gmail.com>
2022-12-26 15:33:14 +0200

    show order tracking

--- PL/Order/OrderTracking.xaml.cs
+++ PL/Order/OrderTracking.xaml.cs
@@ -1,14 +1,29 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-namespace PL.Order;
-
-/// <summary>
-/// Interaction logic for OrderTracking.xaml
-/// </summary>
-public partial class OrderTracking : Window
+namespace PL.Order
 {
-    public OrderTracking()
+    /// <summary>
+    /// Interaction logic for OrderTracking.xaml
+    /// </summary>
+    public partial class OrderTracking : Window
     {
-        InitializeComponent();
+        public OrderTracking()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderDetails(object sender, RoutedEventArgs e) => new Order.Show();
     }
 }
