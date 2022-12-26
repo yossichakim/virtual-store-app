8dc9efd83c85c35c4711ba43432bb60ddccf28c4
Benjamin Machlev <beni251@gmail.com>
2022-12-26 15:33:55 +0200

    edit the track window for costumer

---
+++ PL/Order/Track.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace PL.Order;
+
+/// <summary>
+/// Interaction logic for OrderTracking.xaml
+/// </summary>
+public partial class Track : Window
+{
+    //private BLApi.IBl? _bl;
+    //private BO.OrderTracking? _orderTracking;
+    public Track()
+    {
+        InitializeComponent();
+    }
+}
