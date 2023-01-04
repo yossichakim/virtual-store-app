153cf43f2490470b19f829df6b896ddeaad9fe89
Benjamin Machlev <beni251@gmail.com>
2023-01-04 21:25:06 +0200

    add window to statistics

---
+++ PL/Order/StatisticsOrders.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace PL.Order;
+using System.Windows;
+
+
+
+/// <summary>
+/// Interaction logic for StatisticsOrders.xaml
+/// </summary>
+public partial class StatisticsOrders : Window
+{
+    private static BLApi.IBl? s_bl = BLApi.Factory.Get();
+
+   
+    public StatisticsOrders()
+    {
+        InitializeComponent();
+        StatisticsOrdersByYear.ItemsSource = s_bl!.Order.StatisticsOrdersByYearGroupBy();
+    }
+}
