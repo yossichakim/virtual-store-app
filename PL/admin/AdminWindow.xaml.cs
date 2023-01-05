1ed493a592342a3d9e093570240ba2dd25414974
Benjamin Machlev <beni251@gmail.com>
2023-01-05 17:24:51 +0200

    add comments

--- PL/admin/AdminWindow.xaml.cs
+++ PL/admin/AdminWindow.xaml.cs
@@ -9,14 +9,32 @@ namespace PL.admin;
 /// </summary>
 public partial class AdminWindow : Window
 {
+    /// <summary>
+    /// constructor for AdminWindow
+    /// </summary>
     public AdminWindow()
     {
         InitializeComponent();
     }
 
+    /// <summary>
+    /// function to open ProductList Window
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ProductList(object sender, RoutedEventArgs e) => new ProductList().Show();
 
+    /// <summary>
+    /// function to open OrderList Window 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void OrderList(object sender, RoutedEventArgs e) => new OrderList().Show();
 
+    /// <summary>
+    /// function to open StatisticsOrders Window 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void StatisticsOrders(object sender, RoutedEventArgs e) => new StatisticsOrders().Show();
 }
